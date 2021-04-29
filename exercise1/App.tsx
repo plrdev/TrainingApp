b397977d1ddd7bdeb7c7102b9d1edbd2e1cc368c
Juha Linnanen <juha.linnanen@gofore.com>
2021-04-29 08:59:06 +0300

    Add exercise 1

---
+++ exercise1/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>React Native Training</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
