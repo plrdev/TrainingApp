41c69a0462d7c39adc67d8bccf4fe9031ffdbe34
Juha Linnanen <juha.linnanen@gofore.com>
2021-05-06 15:49:54 +0300

    Update readme

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './exercise2/solution/2.4/App24';
+import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
